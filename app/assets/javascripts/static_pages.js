2dda545d5471a13bdd17fa5fc33a422723e75d42
Peter Furey <prfurey@gmail.com>
2015-06-17 11:41:38 -0600

    Add jquery ui files to app

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+
+  if ($("#accordion").length > 0) {
+
+    $(function() {
+        $("#accordion").accordion({
+          collapsible: true
+        });
+      });
+
+  }
+
+});
