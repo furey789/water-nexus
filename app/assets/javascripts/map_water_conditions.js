aed621ac7772d147203a08966040ee2b64d61943
Peter Furey <prfurey@gmail.com>
2015-06-23 20:22:15 -0600

    Modify water conditions page (e.g. addprogress bar), modify farm economics page (e.g. add image), refactor water conditions js file

--- app/assets/javascripts/map_water_conditions.js
+++ app/assets/javascripts/map_water_conditions.js
@@ -7,156 +7,29 @@ $( document ).ready(function() {
 
   if ($("#map-canvas").length > 0) {
 
-    $(function() {
-        $( "#progressbar" ).progressbar({
-          value: false
-        });
-        progressbar = $( "#progressbar" );
-        progressbarValue = progressbar.find( ".ui-progressbar-value" );
-        progressbarValue.css({"background": 'blue'});
+    function makeBaseMap(){
 
-        // $( "button" ).on( "click", function( event ) {
-        //   var target = $( event.target ),
-        //     progressbar = $( "#progressbar" ),
-        //     progressbarValue = progressbar.find( ".ui-progressbar-value" );
-        //
-        //   if ( target.is( "#numButton" ) ) {
-        //     progressbar.progressbar( "option", {
-        //       value: Math.floor( Math.random() * 100 )
-        //     });
-        //   } else if ( target.is( "#colorButton" ) ) {
-        //     progressbarValue.css({
-        //       "background": '#' + Math.floor( Math.random() * 16777215 ).toString( 16 )
-        //     });
-        //   } else if ( target.is( "#falseButton" ) ) {
-        //     progressbar.progressbar( "option", "value", false );
-        //   }
-        // });
-      });
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+        { center: { lat: 39, lng: -121 },
+          zoom: 7,
+          mapTypeId: google.maps.MapTypeId.SATELLITE,  //SATELLITE, HYBRID
+          zoomControl: true,
+          zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.RIGHT_TOP
+          },
+          streetViewControl: false,
+          panControl: true,
+          panControlOptions: {
+            // style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.RIGHT_TOP
+          }
+        }
+      );
 
-    // console.log('loaded');
-
-    makeBaseMap();
-
-    var today = new Date;
-    var today = [ today.getFullYear()-1, today.getMonth()+1, today.getDate() ]; // current month, prior year
-    var todayString = today.map(function(elem){
-      if (elem <= 9) {
-        elem = '0' + elem.toString();
-      } else {
-        elem = elem.toString();
-      };
-      return elem;
-    })
-    todayString = todayString.join("-");
-
-    var fiveYrsAgo = (todayString.slice(0,4) - 5).toString();
-    var fiveYrsAgoString = fiveYrsAgo + todayString.slice(4,todayString.length);
-
-
-    var endpointNow =
-    'http://waterservices.usgs.gov/nwis/dv/?format=json,1.1&bBox=-124.25,36.75,-118.50,42.10&siteType=LK&startDT=' +
-    todayString + '&endDT=' + todayString;
-
-    var endpointThen =
-    'http://waterservices.usgs.gov/nwis/dv/?format=json,1.1&bBox=-124.25,36.75,-118.50,42.10&siteType=LK&startDT=' +
-    fiveYrsAgoString + '&endDT=' + fiveYrsAgoString;
-
-    // var endpointNow =
-    // 'http://waterservices.usgs.gov/nwis/dv/?format=json,1.1&bBox=-124.25,36.75,-118.50,42.10&startDT=' +
-    // todayString + '&endDT=' + todayString;
-    //
-    // var endpointThen =
-    // 'http://waterservices.usgs.gov/nwis/dv/?format=json,1.1&bBox=-124.25,36.75,-118.50,42.10&startDT=' +
-    // fiveYrsAgoString + '&endDT=' + fiveYrsAgoString;
-
-
-    $.ajax({
-
-      type: 'GET',
-      url: endpointThen,
-      dataType: "json",
-      success: function(dataThen){
-
-        var sitesDataForMap=[];
-        var sitesDataThen = dataThen["value"]["timeSeries"];
-
-        for (var i = 0; i < sitesDataThen.length; i++) {
-
-          var siteDataThen = getSiteInfo(sitesDataThen[i]);
-
-          if ( siteDataThen.measurement.length > 0) {
-            sitesDataForMap.push(
-              { "latitude": siteDataThen.latitude,
-                "longitude": siteDataThen.longitude,
-                "valueThen": siteDataThen.measurement[1] }
-            );
-          };
-
-        };
-
-        // console.log("Num points");
-        // console.log(sitesDataForMap.length);
-
-
-        $.ajax({
-
-          type: 'GET',
-          url: endpointNow,
-          dataType: "json",
-          success: function(dataToday){
-
-            var sitesDataToday = dataToday["value"]["timeSeries"];
-
-            for (var i = 0; i < sitesDataForMap.length; i++) {
-
-              for (var j = 0; j < sitesDataToday.length; j++) {
-
-                var siteDataToday = getSiteInfo(sitesDataToday[j]);
-
-                if ( sitesDataForMap[i]["latitude"] === siteDataToday["latitude"] &&
-                     sitesDataForMap[i]["longitude"] === siteDataToday["longitude"] &&
-                     siteDataToday.measurement.length > 0) {
-
-                    sitesDataForMap[i]["valueNow"] = siteDataToday.measurement[1];
-                    sitesDataForMap[i]["unit"] = siteDataToday.unit;
-                    //console.log(siteDataToday.measurement[1]);
-                    //j = sitesDataToday.length;
-
-                  };
-
-              };
-
-            };
-
-            sitesDataForMap = sitesDataForMap.filter(function(elem){
-              if ( Object.keys(elem).indexOf("valueNow") != -1 ){
-                return elem;
-              };
-            });
-
-            //console.log(sitesDataForMap);
-            for (var i = 0; i < sitesDataForMap.length; i++) {
-              console.log( sitesDataForMap[i].valueThen );
-              console.log( "now", sitesDataForMap[i].valueNow );
-              console.log( "diff", sitesDataForMap[i].valueThen-sitesDataForMap[i].valueNow);
-              console.log( "unit", sitesDataForMap[i].unit);
-
-              // console.log( (sitesDataForMap[i].valueThen-sitesDataForMap[i].valueNow)/sitesDataForMap[i].valueThen );
-              // console.log( (sitesDataForMap[i].valueThen-sitesDataForMap[i].valueNow) );
-              // console.log( sitesDataForMap[i].valueNow );
-            }
-
-            makeHeatMap(sitesDataForMap);
-
-          }  // no semi-colon
-
-        });
-
-      }  // no semi-colon
-
-    });
-
+      google.maps.event.addDomListener(window, 'load', makeBaseMap);
+      //makeBaseMap();
+    }
 
     function getSiteInfo(siteData){
 
@@ -186,32 +59,7 @@ $( document ).ready(function() {
 
     }
 
-
-    function makeBaseMap(){
-
-      var map = new google.maps.Map(document.getElementById('map-canvas'),
-        { center: { lat: 39, lng: -121 },
-          zoom: 7,
-          mapTypeId: google.maps.MapTypeId.SATELLITE,  //SATELLITE, HYBRID
-          zoomControl: true,
-          zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.SMALL,
-            position: google.maps.ControlPosition.RIGHT_TOP
-          },
-          streetViewControl: false,
-          panControl: true,
-          panControlOptions: {
-            // style: google.maps.ZoomControlStyle.SMALL,
-            position: google.maps.ControlPosition.RIGHT_TOP
-          }
-        }
-      );
-
-      google.maps.event.addDomListener(window, 'load', makeBaseMap);
-      //makeBaseMap();
-    }
-
-    function makeHeatMap(siteInfo) {
+    function makeHeatMap(siteInfo){
 
       var map = new google.maps.Map(document.getElementById('map-canvas'),
         { center: { lat: 39, lng: -121 },
@@ -265,25 +113,166 @@ $( document ).ready(function() {
 
       heatmap.setMap(map);  //** THIS LINE PROBLEMATIC OR DOMLISTENER ONE
 
-    //   //bounds of the desired area
-    //   // var allowedBounds = new google.maps.LatLngBounds(
-    //   //      new google.maps.LatLng(37.0,-121.15),
-    //   //      new google.maps.LatLng(39.15,-118.0)
-    //   // );
-    //   // var lastValidCenter = map.getCenter();
-    //   //
-    //   // google.maps.event.addListener(map, 'center_changed', function() {
-    //   //     if (allowedBounds.contains(map.getCenter())) {
-    //   //         // still within valid bounds, so save the last valid position
-    //   //         lastValidCenter = map.getCenter();
-    //   //         return;
-    //   //     }
-    //   //     // not valid anymore => return to last valid position
-    //   //     map.panTo(lastValidCenter);
-    //   //     // map.setCenter(lastValidCenter);
-    //   // });
-    //
+      $( "#progressbar" ).replaceWith( "<div id='progressbar'></div>" );
+
+      $( "#heatmapText" ).replaceWith( "<div id='heatmapText'>Areas in <FONT COLOR='red'><b>red</b></FONT> show that lake levels have declined. This is consistent with California's drought that started roughly in 2011.</div>" );
+
     }
+
+    function progressBar(){
+      $( "#progressbar" ).progressbar({
+        value: false
+      });
+      progressbar = $( "#progressbar" ).height(10);
+      progressbarValue = progressbar.find( ".ui-progressbar-value" );
+      progressbarValue.css({"background": '#6699FF'});   //#9bcc60, #6699FF
+    }
+
+    makeBaseMap();
+
+    $("button").on("click", function(){
+
+      progressBar();
+
+      var today = new Date;
+      var today = [ today.getFullYear()-1, today.getMonth()+1, today.getDate() ]; // current month, prior year
+      var todayString = today.map(function(elem){
+        if (elem <= 9) {
+          elem = '0' + elem.toString();
+        } else {
+          elem = elem.toString();
+        };
+        return elem;
+      })
+      todayString = todayString.join("-");
+
+      var fiveYrsAgo = (todayString.slice(0,4) - 5).toString();
+      var fiveYrsAgoString = fiveYrsAgo + todayString.slice(4,todayString.length);
+
+
+      var endpointNow =
+      'http://waterservices.usgs.gov/nwis/dv/?format=json,1.1&bBox=-124.25,36.75,-118.50,42.10&siteType=LK&startDT=' +
+      todayString + '&endDT=' + todayString;
+
+      var endpointThen =
+      'http://waterservices.usgs.gov/nwis/dv/?format=json,1.1&bBox=-124.25,36.75,-118.50,42.10&siteType=LK&startDT=' +
+      fiveYrsAgoString + '&endDT=' + fiveYrsAgoString;
+
+      // var endpointNow =
+      // 'http://waterservices.usgs.gov/nwis/dv/?format=json,1.1&bBox=-124.25,36.75,-118.50,42.10&startDT=' +
+      // todayString + '&endDT=' + todayString;
+      //
+      // var endpointThen =
+      // 'http://waterservices.usgs.gov/nwis/dv/?format=json,1.1&bBox=-124.25,36.75,-118.50,42.10&startDT=' +
+      // fiveYrsAgoString + '&endDT=' + fiveYrsAgoString;
+
+
+      $.ajax({
+
+        type: 'GET',
+        url: endpointThen,
+        dataType: "json",
+        success: function(dataThen){
+
+          var sitesDataForMap=[];
+          var sitesDataThen = dataThen["value"]["timeSeries"];
+
+          for (var i = 0; i < sitesDataThen.length; i++) {
+
+            var siteDataThen = getSiteInfo(sitesDataThen[i]);
+
+            if ( siteDataThen.measurement.length > 0) {
+              sitesDataForMap.push(
+                { "latitude": siteDataThen.latitude,
+                  "longitude": siteDataThen.longitude,
+                  "valueThen": siteDataThen.measurement[1] }
+              );
+            };
+
+          };
+
+          // console.log("Num points");
+          // console.log(sitesDataForMap.length);
+
+          $.ajax({
+
+            type: 'GET',
+            url: endpointNow,
+            dataType: "json",
+            success: function(dataToday){
+
+              var sitesDataToday = dataToday["value"]["timeSeries"];
+
+              for (var i = 0; i < sitesDataForMap.length; i++) {
+
+                for (var j = 0; j < sitesDataToday.length; j++) {
+
+                  var siteDataToday = getSiteInfo(sitesDataToday[j]);
+
+                  if ( sitesDataForMap[i]["latitude"] === siteDataToday["latitude"] &&
+                       sitesDataForMap[i]["longitude"] === siteDataToday["longitude"] &&
+                       siteDataToday.measurement.length > 0) {
+
+                      sitesDataForMap[i]["valueNow"] = siteDataToday.measurement[1];
+                      sitesDataForMap[i]["unit"] = siteDataToday.unit;
+                      //console.log(siteDataToday.measurement[1]);
+                      //j = sitesDataToday.length;
+
+                    };
+
+                };
+
+              };
+
+              sitesDataForMap = sitesDataForMap.filter(function(elem){
+                if ( Object.keys(elem).indexOf("valueNow") != -1 ){
+                  return elem;
+                };
+              });
+
+              //console.log(sitesDataForMap);
+              for (var i = 0; i < sitesDataForMap.length; i++) {
+                console.log( sitesDataForMap[i].valueThen );
+                console.log( "now", sitesDataForMap[i].valueNow );
+                console.log( "diff", sitesDataForMap[i].valueThen-sitesDataForMap[i].valueNow);
+                console.log( "unit", sitesDataForMap[i].unit);
+
+                // console.log( (sitesDataForMap[i].valueThen-sitesDataForMap[i].valueNow)/sitesDataForMap[i].valueThen );
+                // console.log( (sitesDataForMap[i].valueThen-sitesDataForMap[i].valueNow) );
+                // console.log( sitesDataForMap[i].valueNow );
+              }
+
+              makeHeatMap(sitesDataForMap);
+
+            }  // no semi-colon
+
+          });
+
+        }  // no semi-colon
+
+      });
+
+    });
+
   };
 
 })
+
+//   //bounds of the desired area
+//   // var allowedBounds = new google.maps.LatLngBounds(
+//   //      new google.maps.LatLng(37.0,-121.15),
+//   //      new google.maps.LatLng(39.15,-118.0)
+//   // );
+//   // var lastValidCenter = map.getCenter();
+//   //
+//   // google.maps.event.addListener(map, 'center_changed', function() {
+//   //     if (allowedBounds.contains(map.getCenter())) {
+//   //         // still within valid bounds, so save the last valid position
+//   //         lastValidCenter = map.getCenter();
+//   //         return;
+//   //     }
+//   //     // not valid anymore => return to last valid position
+//   //     map.panTo(lastValidCenter);
+//   //     // map.setCenter(lastValidCenter);
+//   // });
+//
