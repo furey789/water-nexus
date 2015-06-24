655473f2251ad4f374b4e83a9809fa1f3a12f957
Peter Furey <prfurey@gmail.com>
2015-06-24 13:46:18 -0600

    Add farm produce csv files go db/farm_produce_data, modify seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,193 +8,196 @@
 
 # ---------
 #
-# require 'csv'
+require 'csv'
 
-# def getAlmondData
-#
-#   data_raw = []
-#   CSV.foreach("/Users/Furey/workspace/capstone/data_farm_produce/TradeTables_almonds.csv") do |row|
-#     if ( row[2] != nil && ( row[2][0] === 'M' || row[2][0] === '2' ) )
-#       data_raw.push(row)
-#     end
-#   end
-#
-#   attributes = ["Market_year","Aug","Sep","Oct","Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul"]
-#   data_col_ids = []
-#
-#   data_raw[0].each_with_index do |col,id|
-#     if ( ["Market year"+"*"].include?(col) || attributes.include?(col) )
-#       data_col_ids.push(id)
-#     end
-#   end
-#
-#   # Take data from 2009 - 2015
-#   data_values = data_raw[1..6]
-#
-#   data_values.each do |row|
-#     Almond.create!("Units": "$/lb")
-#     data_col_ids.each_with_index do |colid,attrid|
-#       Almond.last.update!("#{attributes[attrid]}": "#{row[colid]}")
-#     end
-#   end
-#
-# end
-#
-# getAlmondData
+def getAlmondData
 
+  data_raw = []
+  # CSV.foreach("/Users/Furey/workspace/capstone/data_farm_produce/TradeTables_almonds.csv") do |row|
+  CSV.foreach("db/farm_produce_data/TradeTables_almonds.csv") do |row|
+    if ( row[2] != nil && ( row[2][0] === 'M' || row[2][0] === '2' ) )
+      data_raw.push(row)
+    end
+  end
 
-# def getWalnutData
-#
-#   data_raw = []
-#   CSV.foreach("/Users/Furey/workspace/capstone/data_farm_produce/TradeTables_walnuts.csv") do |row|
-#     if ( row[2] != nil && ( row[2][0] === 'M' || row[2][0] === '2' ) )
-#       data_raw.push(row)
-#     end
-#   end
-#
-#   attributes = ["Market_year","Aug","Sep","Oct","Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul"]
-#   data_col_ids = []
-#
-#   data_raw[0].each_with_index do |col,id|
-#     if ( ["Market year"+"*"].include?(col) || attributes.include?(col) )
-#       data_col_ids.push(id)
-#     end
-#   end
-#
-#   # Take data from 2009 - 2015
-#   data_values = data_raw[1..6]
-#
-#   data_values.each do |row|
-#     Walnut.create!("Units": "$/lb")
-#     data_col_ids.each_with_index do |colid,attrid|
-#       Walnut.last.update!("#{attributes[attrid]}": "#{row[colid]}")
-#     end
-#   end
-#
-# end
-#
-# getWalnutData
+  attributes = ["Market_year","Aug","Sep","Oct","Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul"]
+  data_col_ids = []
+
+  data_raw[0].each_with_index do |col,id|
+    if ( ["Market year"+"*"].include?(col) || attributes.include?(col) )
+      data_col_ids.push(id)
+    end
+  end
+
+  # Take data from 2009 - 2015
+  data_values = data_raw[1..6]
+
+  data_values.each do |row|
+    Almond.create!("Units": "$/lb")
+    data_col_ids.each_with_index do |colid,attrid|
+      Almond.last.update!("#{attributes[attrid]}": "#{row[colid]}")
+    end
+  end
+
+end
+
+getAlmondData
 
 
-# def getGrapeData
-#
-#   data_raw = []
-#   CSV.foreach("/Users/Furey/workspace/capstone/data_farm_produce/TradeTables_grapes.csv") do |row|
-#     if ( row[2] != nil && ( row[2][0] === 'M' || row[2][0] === '2' ) )
-#       data_raw.push(row)
-#     end
-#   end
-#
-#   attributes = ["Market_year","Aug","Sep","Oct","Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul"]
-#   data_col_ids = []
-#
-#   data_raw[0].each_with_index do |col,id|
-#     if ( ["Market year"+"*"].include?(col) || attributes.include?(col) )
-#       data_col_ids.push(id)
-#     end
-#   end
-#
-#   # Take data from 2009 - 2015
-#   data_values = data_raw[1..6]
-#
-#   data_values.each do |row|
-#     # p row
-#     Grape.create!("Units": "$/lb")
-#     data_col_ids.each_with_index do |colid,attrid|
-#       Grape.last.update!("#{attributes[attrid]}": "#{row[colid]}")
-#     end
-#   end
-#
-# end
-#
-# getGrapeData
+def getWalnutData
+
+  data_raw = []
+  # CSV.foreach("/Users/Furey/workspace/capstone/data_farm_produce/TradeTables_walnuts.csv") do |row|
+  CSV.foreach("db/farm_produce_data/TradeTables_walnuts.csv") do |row|
+    if ( row[2] != nil && ( row[2][0] === 'M' || row[2][0] === '2' ) )
+      data_raw.push(row)
+    end
+  end
+
+  attributes = ["Market_year","Aug","Sep","Oct","Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul"]
+  data_col_ids = []
+
+  data_raw[0].each_with_index do |col,id|
+    if ( ["Market year"+"*"].include?(col) || attributes.include?(col) )
+      data_col_ids.push(id)
+    end
+  end
+
+  # Take data from 2009 - 2015
+  data_values = data_raw[1..6]
+
+  data_values.each do |row|
+    Walnut.create!("Units": "$/lb")
+    data_col_ids.each_with_index do |colid,attrid|
+      Walnut.last.update!("#{attributes[attrid]}": "#{row[colid]}")
+    end
+  end
+
+end
+
+getWalnutData
 
 
-# def getPistachioData
-#
-#   data_raw = []
-#   CSV.foreach("/Users/Furey/workspace/capstone/data_farm_produce/TradeTables_pistachios.csv") do |row|
-#     if ( row[2] != nil && ( row[2][0] === 'M' || row[2][0] === '2' ) )
-#       data_raw.push(row)
-#     end
-#   end
-#
-#   attributes = ["Market_year","Aug","Sep","Oct","Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul"]
-#   data_col_ids = []
-#
-#   data_raw[0].each_with_index do |col,id|
-#     if ( ["Market year"+"*"].include?(col) || attributes.include?(col) )
-#       data_col_ids.push(id)
-#     end
-#   end
-#
-#   # Take data from 2009 - 2015
-#   data_values = data_raw[1..6]
-#
-#   data_values.each do |row|
-#     p row
-#     Pistachio.create!("Units": "$/lb")
-#     data_col_ids.each_with_index do |colid,attrid|
-#       Pistachio.last.update!("#{attributes[attrid]}": "#{row[colid]}")
-#     end
-#   end
-#
-# end
-#
-# getPistachioData
+def getGrapeData
 
-# def getDatepalmData
-#
-#   data_raw = []
-#   CSV.foreach("/Users/Furey/workspace/capstone/data_farm_produce/TradeTables_datepalms.csv") do |row|
-#     if ( row[2] != nil && ( row[2][0] === 'M' || row[2][0] === '2' ) )
-#       data_raw.push(row)
-#     end
-#   end
-#
-#   attributes = ["Market_year","Aug","Sep","Oct","Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul"]
-#   data_col_ids = []
-#
-#   data_raw[0].each_with_index do |col,id|
-#     if ( ["Market year"+"*"].include?(col) || attributes.include?(col) )
-#       data_col_ids.push(id)
-#     end
-#   end
-#
-#   # Take data from 2009 - 2015
-#   data_values = data_raw[1..6]
-#
-#   data_values.each do |row|
-#     # p row
-#     Datepalm.create!("Units": "$/lb")
-#     data_col_ids.each_with_index do |colid,attrid|
-#       Datepalm.last.update!("#{attributes[attrid]}": "#{row[colid]}")
-#     end
-#   end
-#
-# end
-#
-# getDatepalmData
+  data_raw = []
+  # CSV.foreach("/Users/Furey/workspace/capstone/data_farm_produce/TradeTables_grapes.csv") do |row|
+  CSV.foreach("db/farm_produce_data/TradeTables_grapes.csv") do |row|
+    if ( row[2] != nil && ( row[2][0] === 'M' || row[2][0] === '2' ) )
+      data_raw.push(row)
+    end
+  end
+
+  attributes = ["Market_year","Aug","Sep","Oct","Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul"]
+  data_col_ids = []
+
+  data_raw[0].each_with_index do |col,id|
+    if ( ["Market year"+"*"].include?(col) || attributes.include?(col) )
+      data_col_ids.push(id)
+    end
+  end
+
+  # Take data from 2009 - 2015
+  data_values = data_raw[1..6]
+
+  data_values.each do |row|
+    Grape.create!("Units": "$/lb")
+    data_col_ids.each_with_index do |colid,attrid|
+      Grape.last.update!("#{attributes[attrid]}": "#{row[colid]}")
+    end
+  end
+
+end
+
+getGrapeData
+
+
+def getPistachioData
+
+  data_raw = []
+  # CSV.foreach("/Users/Furey/workspace/capstone/data_farm_produce/TradeTables_pistachios.csv") do |row|
+  CSV.foreach("db/farm_produce_data/TradeTables_pistachios.csv") do |row|
+    if ( row[2] != nil && ( row[2][0] === 'M' || row[2][0] === '2' ) )
+      data_raw.push(row)
+    end
+  end
+
+  attributes = ["Market_year","Aug","Sep","Oct","Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul"]
+  data_col_ids = []
+
+  data_raw[0].each_with_index do |col,id|
+    if ( ["Market year"+"*"].include?(col) || attributes.include?(col) )
+      data_col_ids.push(id)
+    end
+  end
+
+  # Take data from 2009 - 2015
+  data_values = data_raw[1..6]
+
+  data_values.each do |row|
+    Pistachio.create!("Units": "$/lb")
+    data_col_ids.each_with_index do |colid,attrid|
+      Pistachio.last.update!("#{attributes[attrid]}": "#{row[colid]}")
+    end
+  end
+
+end
+
+getPistachioData
+
+
+def getDatepalmData
+
+  data_raw = []
+  # CSV.foreach("/Users/Furey/workspace/capstone/data_farm_produce/TradeTables_datepalms.csv") do |row|
+  CSV.foreach("db/farm_produce_data/TradeTables_datepalms.csv") do |row|
+    if ( row[2] != nil && ( row[2][0] === 'M' || row[2][0] === '2' ) )
+      data_raw.push(row)
+    end
+  end
+
+  attributes = ["Market_year","Aug","Sep","Oct","Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul"]
+  data_col_ids = []
+
+  data_raw[0].each_with_index do |col,id|
+    if ( ["Market year"+"*"].include?(col) || attributes.include?(col) )
+      data_col_ids.push(id)
+    end
+  end
+
+  # Take data from 2009 - 2015
+  data_values = data_raw[1..6]
+
+  data_values.each do |row|
+    Datepalm.create!("Units": "$/lb")
+    data_col_ids.each_with_index do |colid,attrid|
+      Datepalm.last.update!("#{attributes[attrid]}": "#{row[colid]}")
+    end
+  end
+
+end
+
+getDatepalmData
 
 
 # # Tumblr
-client = Tumblr::Client.new
-hash = client.posts("stateofdrought.tumblr.com")
+# client = Tumblr::Client.new
+# hash = client.posts("stateofdrought.tumblr.com")
 
 # p hash["blog"]
 # p hash["posts"].length
 
 # post_ids = TumblrBlog.pluck("post_id")
 
-p hash["posts"].length
+# p hash["posts"].length
 # p post_ids
 
-hash["posts"].each do |post|
+# hash["posts"].each do |post|
 
   # if (post_ids.index(post["id"]) != nil)
 
-    p post["id"]
-    p post["date"]
+    # p post["id"]
+    # p post["date"]
 
     # TumblrBlog.create!(
     #   "post_id": "#{post["id"]}",
@@ -204,14 +207,4 @@ hash["posts"].each do |post|
 
   # end
 
-end
-
-
-## Water Site Info
-# WaterSite.create!("name": "Lake Shasta", "code": "11370000")
-# WaterSite.create!("name": "Lake Trinity", "code": "11525400")
-# WaterSite.create!("name": "Lake Oroville", "code": "11406800")
-# WaterSite.create!("name": "Folsom Lake", "code": "11446200")
-# WaterSite.create!("name": "New Melones Reservoir", "code": "11299000")
-# WaterSite.create!("name": "Don Pedro Reservoir", "code": "11287500")
-# WaterSite.create!("name": "Lake Berryessa", "code": "11453900")
+# end
